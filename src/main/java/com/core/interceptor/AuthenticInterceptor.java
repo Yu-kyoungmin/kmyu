8c5055204ad877dbc839f6a812b0f696cb68d178
LK-MIN <LK-MIN@DESKTOP-FKS40CH>
2022-04-29 16:34:46 +0900

    수정

---
+++ src/main/java/com/core/interceptor/AuthenticInterceptor.java
@@ -0,0 +1,22 @@
+package com.core.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * 세션 체크 인터셉터.
+ * @author KMYU
+ * @since 2022. 4. 29
+ * @version 1.0 
+ * @see 
+ * 
+ * <pre> 
+ * << 개정이력(Modification Information) >> 
+ * 
+ * 수정일                       수정자                 수정내용 
+ * -------     --------  --------------------------- 
+ *  
+ * </pre>
+ */
+public class AuthenticInterceptor extends HandlerInterceptorAdapter {
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,1 +1,14 @@
-aaaccccccccaaasfsdfff
+<%--##############################################################################
+# Filename    : index.jsp
+# Description : Index 페이지
+# Date        : 
+# Author      : 
+##################################################################################--%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		test
+	</body>
+</html>
